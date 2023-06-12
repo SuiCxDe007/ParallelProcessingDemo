d70d0587ec3054a7bf54b03b54ccbbe0933c154b
SuiCxDe <nitroxcyanide@gmail.com>
2023-06-13 00:11:02 +0530

    Initial commit

---
+++ src/main/java/com/suicxde/parallelprocessingdemo/ParallelProcessingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.suicxde.parallelprocessingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParallelProcessingDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParallelProcessingDemoApplication.class, args);
+    }
+
+}
